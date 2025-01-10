5e666e62cf2becd03518e53d16050cd1630cdb4d
mrkwab3 <mrkwab3@gmail.com>
2025-01-09 22:35:36 -0500

    Completed part 1 and 2

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ const n4 = 5;
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
 const isSum50 = (n1 + n2 + n3 + n4) == 50;
+console.log(`The four numbers meet the criteria: ${isSum50}`)
 
 // Check two: at least two odd numbers
 // Here, we use modulus to check if something is odd.
 // Since % 2 is 0 if even and 1 if odd, we can use
 // arithmetic to count the total number of odd numbers.
 const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+console.log(`The four numbers meet the criteria: ${isTwoOdd}`)
 
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
@@ -26,6 +28,7 @@ let isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
 // ways of handling it with other data structures
 // that we will review later.
 const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+console.log(`The four numbers meet the criteria: ${isUnique}`)
 
 // Here, we put the results into a single variable 
 // for convenience. Note how we negate isOver25 using
@@ -34,7 +37,7 @@ const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 
 const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
 
 // Finally, log the results.
-console.log(isValid);
+console.log(`The four numbers meet the criteria: ${isValid}`);
 
 // Here's another example of how this COULD be done,
 // but it SHOULD NOT be done this way. As programmers,
@@ -44,21 +47,70 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+console.log(`The four numbers meet the criteria: ${dontDoThis}`)
 
 // Checks if numbers are divisible by 5
 const isDivisibleBy5 = (n1 / 5 == 0) && (n2 / 5 == 0) && (n3 / 5 == 0) && ((n4 / 5 == 0))
+console.log(`The four numbers meet the criteria: ${isDivisibleBy5}`)
 
 // Checks if the first number is larger than the last number
 const firstLargerThanLast = n1 > n4
+console.log(`The four numbers meet the criteria: ${firstLargerThanLast}`)
 
 // Evaluates the arithmetic chain
 const arithmeticChain = ((n2 - n1) * 3) % n4
+console.log(`The four numbers meet the criteria: ${arithmeticChain}`)
 
-// Changes isOver25 to not use the 'negate' operator (changed previous iteration to 'let' instead of 'const')
-// ~ A bit confused on this one ~
+// Changes isOver25 to not use the 'negate' operator in future iterations (changed previous iteration to 'let' instead of 'const')
+let isUnder25 = n1 <= 25 || n2 <= 25 || n3 <= 25 || n4 <= 25;
+isOver25 = !isUnder25
 
 // Part 2:
+const distance = 1500
+const budget = 175
+const avgFuelCost = 3
+let mph;
+let mpg;
 
+  // 55 mph
+  {
+    mph = 55
+    mpg = 30
+    const gallonsNeeded = distance / mpg
+    const enoughBudget = (gallonsNeeded * 3) < budget
+    const tripTime = distance / mph
+    console.log
+    (`During this trip I am travelling ${mph} mph with a mileage of ${mpg} mpg.
+    I will need ${gallonsNeeded} gallons for this trip. 
+    It is ${enoughBudget} that I have enough of a budget for this trip.
+    This trip will take ${tripTime} hours to complete.`)
+  }
+  // 60 mph
+  {
+    mph = 60
+    mpg = 28
+    const gallonsNeeded = distance / mpg
+    const enoughBudget = (gallonsNeeded * 3) < budget
+    const tripTime = distance / mph
+    console.log
+    (`During this trip I am travelling ${mph} mph with a mileage of ${mpg} mpg.
+    I will need ${gallonsNeeded} gallons for this trip. 
+    It is ${enoughBudget} that I have enough of a budget for this trip.
+    This trip will take ${tripTime} hours to complete.`)
+  }
+  // 75 mph
+  {
+    mph = 75
+    mpg = 23
+    const gallonsNeeded = distance / mpg
+    const enoughBudget = (gallonsNeeded * 3) < budget
+    const tripTime = distance / mph
+    console.log
+    (`During this trip I am travelling ${mph} mph with a mileage of ${mpg} mpg.
+    I will need ${gallonsNeeded} gallons for this trip. 
+    It is ${enoughBudget} that I have enough of a budget for this trip.
+    This trip will take ${tripTime} hours to complete.`)
+  }
 // Part 3:
 
 // Part 4:
